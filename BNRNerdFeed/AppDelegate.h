28a98ad1e4e403664e928b1a43ff5eebd504fbf7
DJ Chung <djchung3@gmail.com>
2012-06-15 15:24:48 -0500

    initial project files

---
+++ BNRNerdFeed/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BNRNerdFeed
+//
+//  Created by DJ Chung on 6/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
